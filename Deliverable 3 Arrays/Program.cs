990e92f0ee7e63e6d55e2eda6cdccc2e01a4928a
Monica Costa <monicacosta@usf.edu>
2019-01-28 22:10:04 -0500

    Update

--- Deliverable 3 Arrays/Program.cs
+++ Deliverable 3 Arrays/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Author: Monica Costa
+Date: 1/28/2019
+Comments: This C# Console application code demonstrates the use of
+  arrays.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,35 @@ namespace Deliverable_3_Arrays
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+
+
+            try
+            {
+                // n is an array of 25 integers
+                int[] array = new int[25];
+                int i, j;
+
+                /* initialize elements of array n */
+                for (i = 1; i < 25; i++)
+                {
+                    array[i] = i;
+                }
+
+                /* output each array element's value */
+                for (j = 1; j < 25; j++)
+                {
+                    Console.WriteLine("Element = {1}", j, array[j]);
+                }
+                Console.ReadKey();
+
+
+            } //End of try
+
+            catch
+            {
+                Console.WriteLine("There was an error");
+                Console.ReadKey(true);
+            } //End of catch
+        } //End of main
+    } //End of class
+} //End of namespace
